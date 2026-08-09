b4943b217d2cb531b214b6f7000e276b6ba8cd3c
akashdeepmaity4 <akashdeepmaity66@gmail.com>
2026-08-09 13:18:43 +0530

    updated

--- remakeexecutable.sh
+++ remakeexecutable.sh
@@ -1,6 +1,15 @@
-cd D:\projects\emeraldnotes
-rmdir /s build
-rmdir /s dist
-del *.spec
+# DISCLAIMER
+cd D:\projects
+# Change this above location to the location you want your codebade to live
+
+
+cd emeraldnotes
 pyinstaller --onefile --windowed --add-data "templates;templates" --add-data "static;static" --add-data "app;app" --add-data "storage;storage" --hidden-import flask --hidden-import jinja2 --hidden-import app --hidden-import app.app --paths . launcher.py
+echo Download complete! Removing duplicacies... 
+
+mv dist/launcher.exe launcher.exe
+mv launcher.exe EmeraldNotes.exe
+rm -rf dist
+rm -rf build
+rm -f *.spec
 echo build complete!
